0854715a3a36d363e0d8ce9680048cbeccc44052
Rodrigo <rhbon.08@gmail.com>
2024-04-03 16:33:54 -0500

    Variables en PHP

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,19 @@
 <?php include 'includes/header.php';
 
+$nombre = "Rodrigo";
 
+
+echo $nombre;
+var_dump($nombre);
+
+define('constante', "Este es el valor de la constante");
+echo constante;
+
+const constante2 = "Hola 2";
+echo constante2;
+
+$nombreCliente = "Pedro";
+$nombre_cliente = "Pedro";
 
 
 include 'includes/footer.php';
