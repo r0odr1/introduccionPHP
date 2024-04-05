a6465a929d6f8288c9f2f9a1a68eb8be315be9cc
Rodrigo <rhbon.08@gmail.com>
2024-04-05 17:22:58 -0500

    tipos de dato en PHP

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,5 +1,24 @@
 <?php include 'includes/header.php';
 
+// Boolean
+$logueado = true;
+var_dump($logueado);
+
+// Enteros
+$numero = 200;
+var_dump($numero);
+
+// Floats
+$float = 200.5;
+var_dump($float);
+
+// String
+$nombre = "Rodrigo"; // Pueden estar dentro de comillas sensillas o dobles pero no se pueden mezclar
+var_dump($nombre);
+
+// Arrays
+$array = [];
+var_dump($array);
 
 
 
