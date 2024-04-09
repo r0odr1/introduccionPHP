65c7a00e03dce705b6881f5e1634cff634213730
Rodrigo <rhbon.08@gmail.com>
2024-04-08 19:06:03 -0500

    Numeros u operadores en php

--- 04-operadores.php
+++ 04-operadores.php
@@ -1,6 +1,26 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 10;
 
+// Sumar
+echo $numero1 + $numero2;
+
+echo "<br>"; // Salto de linea
+// Resta
+echo $numero1 - $numero2;
+echo "<br>"; // Salto de linea
+
+// Multiplicar
+echo $numero1 * $numero2;
+echo "<br>"; // Salto de linea
+
+// Dividir
+echo $numero1 / $numero2;
+echo "<br>"; // Salto de linea
+
+// Multiplicar cierta cantidad de veces
+echo 2 ** 3;
 
 
 include 'includes/footer.php';
