621fc124ec50ef7760b3460e65d366b0fead084e
Rodrigo <rhbon.08@gmail.com>
2024-04-09 17:39:20 -0500

    Arreglos asociativos o Associative Arrays en PHP

--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,5 +1,31 @@
 <?php include 'includes/header.php';
 
+$cliente = [
+  'nombre' => 'Rodrigo',
+  'saldo' => 200,
+  'informacion' => [
+    'tipo' => 'premium',
+    'disponible' => 100
+  ]
+];
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+
+// echo $cliente['nombre'];
+// echo "<br>";
+
+// echo $cliente['saldo'];
+// echo "<br>";
+
+// echo $cliente['informacion']['disponible'];
+
+$cliente['codigo'] = 123456789;
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 
 
