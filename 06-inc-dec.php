1e249eb68c019443f1afa25134e7be9e5a09f649
Rodrigo <rhbon.08@gmail.com>
2024-04-09 16:27:51 -0500

    incrementos en PHP

--- 06-inc-dec.php
+++ 06-inc-dec.php
@@ -1,5 +1,17 @@
 <?php include 'includes/header.php';
 
+$numero1 = 30;
+//$numero1++;
+echo ++$numero1;
+echo "<br>";
+
+echo $numero1 += 5;
+echo $numero1;
+echo "<br>";
+
+$numero2 = 30;
+//$numero2--;
+echo --$numero2;
 
 
 
