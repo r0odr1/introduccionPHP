293d698c5944b8cd6b39b8d1303ad476a4d07fb4
Rodrigo <rhbon.08@gmail.com>
2024-04-09 19:08:05 -0500

    isset() y empty()

--- 10-isset-empty.php
+++ 10-isset-empty.php
@@ -1,6 +1,28 @@
 <?php include 'includes/header.php';
 
+$clientes = [];
+$clientes2 = array();
+$clientes3 = array('Pedro', 'Juan', 'Karen');
+$cliente = [
+  'nombre' => 'Rodrigo',
+  'saldo' => 200
+];
 
+// Empty: Validamos si un arreglo esta vacio
+var_dump( empty($clientes) );
+var_dump( empty($clientes3) );
+var_dump( empty($clientes2) );
+
+// Isset: Revisa si una propiedad esta definida o arreglo esta creado
+echo "<br>";
+var_dump( isset($clientes4));
+var_dump( isset($clientes));
+var_dump( isset($clientes2));
+var_dump( isset($clientes3));
+
+// Isset: Permite revisar si una propiedad de un arreglo asociativo existe
+var_dump(isset($cliente['nombre']));
+var_dump(isset($cliente['codigo']));
 
 
 include 'includes/footer.php';
