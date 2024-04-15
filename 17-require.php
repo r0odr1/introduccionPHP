862fd77311b736d4b706319fb51505e645fef595
Rodrigo <rhbon.08@gmail.com>
2024-04-15 18:36:14 -0500

    include y require

--- 17-require.php
+++ 17-require.php
@@ -1,7 +1,14 @@
 <?php include 'includes/header.php';
+// otra manera de importar archivos es con la palabra "require"
 
+// 1. Require: Se utiliza cuando tenga funciones, si no encuentra el archivo para la ejecucion
+// 2. Include: Se utiliza cuando quiera incluir otros tamplate, si no encuentra el archivo sigue ejecutandose
+// 3. Require_once: incluye una sola vez, primero revisa si el archivo ya fue incluido,
+// si ya fue incluido ignora "require_once", si no ha sido incluido lo va a incluir
 
+require 'funciones.php';
 
+iniciarApp();
 
 
 
